b22c14e9ac6afd2932d938307a4f37ec723bf028
nguyendat2741998 <nguyendat.2741998@gmail.com>
2020-10-21 21:39:19 +0700

    factory create instance

--- obbin.js
+++ obbin.js
@@ -1,175 +1,41 @@
-const ObbinJS = function () {
-    this._values = {};
-    const that = this;
-    this.watch = function (key, callback, timeout = 0) {
-        const v = this.find(key);
-        if (!!v) v.events.push({
-            callback: callback,
-            timeout: timeout, instance: null
-        });
-        else throw `${key} realy define`;
-    };
-    this.validators = function (key, validators = []) {
-        const v = this.find(key);
-        if (!!v) v.validators = validators;
-        else throw `${key} realy define`;
-    };
-    this.set = function (key, value, commit = true) {
-        const v = this.find(key);
-        if (!!v) {
-            let valid = this._validators.run(value, v.validators);
-            if (!valid) {
-                throw 'value is not validate';
-            } else {
-                const oldValue = v.value;
-                v.value = value;
-                if (commit) this._events.call(v.events, v.value, oldValue);
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['exports', 'b'], factory);
+    } else if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
+        factory(exports, require('b'));
+    } else {
+        factory((root.ObbinJS = {}), root.b);
+    }
+}(this, function (exports, b) {
+    // the exported module properties.
+    exports._values = {};
+
+    exports._validators = {
+        run: function (value, validators = []) {
+            const checker = []
+            for (let index = 0; index < validators.length; index++) {
+                let v = validators[index];
+                checker.push(v(value));
             }
-        }
-        else {
-            const newValue = this._format.getValueFormat();
-            newValue.value = value;
-            this._values[key] = newValue;
-        }
-    };
-    this.get = function (key) {
-        const v = this.find(key);
-        return !!v ? v.value : null;
-    },
-        this.find = function (key) {
-            const keys = Object.keys(this._values);
-            if (keys.includes(key)) return this._values[key];
-            return null;
-        };
-    this.live = function (key, selector, render) {
-        const v = this.find(key);
-        if (!!v) {
-            this.watch(key, function (data, old) {
-                var elements = [];
-                const html = render(data, old);
-                const hasLocker = that._locker.hasLocker(selector);
-
-                if (hasLocker) {
-                    elements = that._query.withLocker(
-                        that._locker.getLocker(selector)
-                    );
-                } else {
-                    elements = that._query.withSelector(selector);
-                }
-
-                const virtual = that._render.parseHTML2DOM(html);
-                that._locker.lockElement(selector, virtual);
-                [].forEach.call(elements, function (el) {
-                    if (el != null) el.replaceWith(virtual);
-                })
-            });
-        }
-        else throw `${key} realy define`;
-    };
-    this.liveDOM = function (key, selector, render) {
-        const v = this.find(key);
-        if (!!v) {
-            this.watch(key, function (data, old) {
-                // get virtual object
-                const context = { newValue: data, oldValue: old };
-                const virtual = render(context, that._render.createElement);
-                // render virtual dom element
-                const virtualDom = that._render.render(virtual);
-
-                if (that._locker.hasLocker(selector)) {
-                    var locker = that._locker.getLocker(selector);
-                    var el = that._query.withLocker(locker);
-
-                    const patch = that._render.diff(locker.virtual, virtual);
-                    locker.dom = patch(el);
-                    locker.virtual = virtual;
-                } else {
-                    var locker = that._locker.lockElement(selector, virtual, virtualDom);
-                    var el = that._query.withSelector(selector);
-
-                    // insert to dom
-                    that._render.insertElement(virtualDom, el);
-                }
-            });
-        }
-        else throw `${key} realy define`;
-    };
-    this.bind = function (key, selector, mapValue, trigger = 'change', delay = 0) {
-        const v = this.find(key);
-        if (!!v) {
-            const elements = this._query.withSelectorAll(selector);
-            const self = this;
-            elements.forEach(function (el) {
-                if (el instanceof HTMLInputElement
-                    || el instanceof HTMLSelectElement) {
-
-                    el.addEventListener(trigger, function (evt) {
-                        self.set(key, evt.target.value)
-                    });
-
-                    self.watch(key, function (obj, old) {
-                        if (!!mapValue) el.value = mapValue(obj);
-                        else el.value = obj;
-                    }, delay);
-                }
-            });
-        }
-        else throw `${key} realy define`;
-    };
-    this._format = {
-        getValueFormat: function () {
-            return { value: null, events: [], validators: [], timeout: null }
-        },
-        getLockerFormat: function () {
-            return { key: null, virtual: null, dom: null }
-        }
-    };
-    this._events = {
-        call: function (events, value, oldValue) {
-            for (let index = 0; index < events.length; index++) {
-                let e = events[index];
-                if (e.timeout == 0) e.callback(value, oldValue);
-                else {
-                    clearTimeout(e.instance);
-                    e.instance = setTimeout(function () {
-                        e.callback(value, oldValue);
-                    }, e.timeout);
-                }
-            }
+            return checker.every(function (valid) {
+                return valid === true;
+            })
         },
     };
-    this._locker = {
-        _lockers: {},
-        _increment: 1,
-        lockElement: function (selector, virtual, dom) {
-            var looker = that._format.getLockerFormat();
 
-            if (!this.hasLocker(selector)) {
-                looker.key = this.incrementId();
-                this._lockers[selector] = looker;
-            } else {
-                looker = this.getLocker(selector);
-            }
-
-            looker.dom = dom;
-            looker.virtual = virtual;
-
-            dom.dataset.sid = looker.key;
-            return looker
+    exports._query = {
+        withLocker: function (locker) {
+            return document.querySelector(`[data-sid="${locker.key}"]`)
         },
-        getLocker: function (selector) {
-            return this._lockers[selector] || null
+        withSelector: function (selector) {
+            return document.querySelector(selector);
         },
-        hasLocker: function (selector) {
-            var looker = this.getLocker(selector);
-            return !(typeof looker === 'undefined' || looker == null)
-        },
-        incrementId: function () {
-            this._increment++;
-            return 'sid-' + this._increment;
+        withSelectorAll: function (selector) {
+            return document.querySelectorAll(selector);
         },
     };
-    this._render = {
+
+    exports._render = {
         _textnodes: ['bigint', 'string', 'number',
             'undefined', 'boolean'],
         parseHTML2DOM: function (html) {
@@ -308,28 +174,158 @@ const ObbinJS = function () {
             return value;
         }
     };
-    this._query = {
-        withLocker: function (locker) {
-            return document.querySelector(`[data-sid="${locker.key}"]`)
+
+    exports._locker = {
+        _lockers: {},
+        _increment: 1,
+        lockElement: function (selector, virtual, dom) {
+            var looker = exports._format.getLockerFormat();
+
+            if (!this.hasLocker(selector)) {
+                looker.key = this.incrementId();
+                this._lockers[selector] = looker;
+            } else {
+                looker = this.getLocker(selector);
+            }
+
+            looker.dom = dom;
+            looker.virtual = virtual;
+
+            dom.dataset.sid = looker.key;
+            return looker
         },
-        withSelector: function (selector) {
-            return document.querySelector(selector);
+        getLocker: function (selector) {
+            return this._lockers[selector] || null
         },
-        withSelectorAll: function (selector) {
-            return document.querySelectorAll(selector);
+        hasLocker: function (selector) {
+            var looker = this.getLocker(selector);
+            return !(typeof looker === 'undefined' || looker == null)
+        },
+        incrementId: function () {
+            this._increment++;
+            return 'sid-' + this._increment;
         },
     };
-    this._validators = {
-        run: function (value, validators = []) {
-            const checker = []
-            for (let index = 0; index < validators.length; index++) {
-                let v = validators[index];
-                checker.push(v(value));
+
+    exports._events = {
+        call: function (events, value, oldValue) {
+            for (let index = 0; index < events.length; index++) {
+                let e = events[index];
+                if (e.timeout == 0) e.callback(value, oldValue);
+                else {
+                    clearTimeout(e.instance);
+                    e.instance = setTimeout(function () {
+                        e.callback(value, oldValue);
+                    }, e.timeout);
+                }
             }
-            return checker.every(function (valid) {
-                return valid === true;
-            })
         },
-    }
-};
-export default ObbinJS;
+    };
+
+    exports._format = {
+        getValueFormat: function () {
+            return { value: null, events: [], validators: [], timeout: null }
+        },
+        getLockerFormat: function () {
+            return { key: null, virtual: null, dom: null }
+        }
+    };
+
+    exports.bind = function (key, selector, mapValue, trigger = 'change', delay = 0) {
+        const v = this.find(key);
+        if (!!v) {
+            const elements = this._query.withSelectorAll(selector);
+            const self = this;
+            elements.forEach(function (el) {
+                if (el instanceof HTMLInputElement
+                    || el instanceof HTMLSelectElement) {
+
+                    el.addEventListener(trigger, function (evt) {
+                        self.set(key, evt.target.value)
+                    });
+
+                    self.watch(key, function (obj, old) {
+                        if (!!mapValue) el.value = mapValue(obj);
+                        else el.value = obj;
+                    }, delay);
+                }
+            });
+        }
+        else throw `${key} realy define`;
+    };
+
+    exports.liveDOM = function (key, selector, render) {
+        const v = this.find(key);
+        if (!!v) {
+            this.watch(key, function (data, old) {
+                // get virtual object
+                const context = { newValue: data, oldValue: old };
+                const virtual = render(context, exports._render.createElement);
+                // render virtual dom element
+                const virtualDom = exports._render.render(virtual);
+
+                if (exports._locker.hasLocker(selector)) {
+                    var locker = exports._locker.getLocker(selector);
+                    var el = exports._query.withLocker(locker);
+
+                    const patch = exports._render.diff(locker.virtual, virtual);
+                    locker.dom = patch(el);
+                    locker.virtual = virtual;
+                } else {
+                    var locker = exports._locker.lockElement(selector, virtual, virtualDom);
+                    var el = exports._query.withSelector(selector);
+
+                    // insert to dom
+                    exports._render.insertElement(virtualDom, el);
+                }
+            });
+        }
+        else throw `${key} realy define`;
+    };
+
+    exports.find = function (key) {
+        const keys = Object.keys(this._values);
+        if (keys.includes(key)) return this._values[key];
+        return null;
+    };
+
+
+    exports.get = function (key) {
+        const v = this.find(key);
+        return !!v ? v.value : null;
+    };
+
+    exports.set = function (key, value, commit = true) {
+        const v = this.find(key);
+        if (!!v) {
+            let valid = this._validators.run(value, v.validators);
+            if (!valid) {
+                throw 'value is not validate';
+            } else {
+                const oldValue = v.value;
+                v.value = value;
+                if (commit) this._events.call(v.events, v.value, oldValue);
+            }
+        }
+        else {
+            const newValue = this._format.getValueFormat();
+            newValue.value = value;
+            this._values[key] = newValue;
+        }
+    };
+
+    exports.validators = function (key, validators = []) {
+        const v = this.find(key);
+        if (!!v) v.validators = validators;
+        else throw `${key} realy define`;
+    };
+
+    exports.watch = function (key, callback, timeout = 0) {
+        const v = this.find(key);
+        if (!!v) v.events.push({
+            callback: callback,
+            timeout: timeout, instance: null
+        });
+        else throw `${key} realy define`;
+    };
+}));
